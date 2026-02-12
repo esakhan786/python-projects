1f7d5e041f0702e874edc239f389ec9bbec31d2c
Esa khan <esak62622@gmail.com>
2026-02-12 22:57:19 +0500

    Added WhatsApp automation project using pywhatkit

---
+++ wtsp.py
@@ -0,0 +1,10 @@
+import pywhatkit as kit
+
+# Phone number with country code 
+phone_number = "+923250070313"  # Replace with the recipient's phone number
+message = "Hello! This message is sent using Python � By code_with_esa"
+
+# Send instantly (opens WhatsApp Web)
+kit.sendwhatmsg_instantly(phone_number, message)
+
+print("Message sent successfully!")
