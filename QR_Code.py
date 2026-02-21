7b03387c61e84f5d816c891719bcc5b87b0955fa
Esa khan <esak62622@gmail.com>
2026-02-22 03:51:57 +0500

    Created QR Code Generator Application using Python

---
+++ QR_Code.py
@@ -0,0 +1,12 @@
+import qrcode as qr
+# img=qr.make("https://net22.cc/home")
+# img.save("Netmirror.png")
+
+
+#My linkdin QR code
+# img=qr.make("https://www.linkedin.com/in/esa-khan-1b4941385/")
+# img.save("Esa khan Linkdin.png")
+
+#My Github QR code
+img=qr.make("https://github.com/esakhan786")
+img.save("Esa khan Github.png")
